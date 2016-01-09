d16df0c37ce1c5705314d48da548178b5998d4a5
eaglesakura <eagle.sakura@gmail.com>
2016-01-09 17:25:54 +0900

    add sources

---
+++ src/main/java/util/SerializeUtil.java
@@ -0,0 +1,134 @@
+package com.eaglesakura.util;
+
+import com.eaglesakura.io.DataInputStream;
+import com.eaglesakura.io.DataOutputStream;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+public class SerializeUtil {
+    /**
+     * ファイル配列をシリアライズする
+     *
+     * @param data
+     * @return
+     */
+    public static byte[] toByteArray(List<byte[]> data) {
+        try {
+            int sumSize = 4;
+            for (byte[] file : data) {
+                sumSize += (file.length + 4);
+            }
+
+            ByteArrayOutputStream os = new ByteArrayOutputStream(sumSize);
+            DataOutputStream dos = new DataOutputStream(os, false);
+
+            dos.writeS32(data.size());
+            Iterator<byte[]> iterator = data.iterator();
+            while (iterator.hasNext()) {
+                byte[] file = iterator.next();
+                dos.writeFile(file);
+            }
+
+            return os.toByteArray();
+        } catch (Exception e) {
+            throw new IllegalStateException();
+        }
+    }
+
+    /**
+     * toByteArrayでシリアライズしたデータをデシリアライズする
+     *
+     * @param buffer
+     * @return
+     */
+    public static List<byte[]> toByteArrayList(byte[] buffer) {
+        return toByteArrayList(buffer, 0, buffer.length);
+    }
+
+    /**
+     * toByteArrayでシリアライズしたデータをデシリアライズする
+     *
+     * @param buffer
+     * @return
+     */
+    public static List<byte[]> toByteArrayList(byte[] buffer, int offset, int length) {
+        try {
+
+            ByteArrayInputStream is = new ByteArrayInputStream(buffer, offset, length);
+            DataInputStream dis = new DataInputStream(is, false);
+
+            List<byte[]> result = new ArrayList<>();
+            final int num = dis.readS32();
+
+            for (int i = 0; i < num; ++i) {
+                result.add(dis.readFile());
+            }
+
+            return result;
+        } catch (Exception e) {
+            LogUtil.log(e);
+            throw new IllegalArgumentException("FormatError");
+        }
+    }
+
+    /**
+     * Key-Valueデータを圧縮する
+     *
+     * @param data
+     * @return
+     */
+    public static byte[] toByteArray(Map<String, byte[]> data) {
+        try {
+            ByteArrayOutputStream os = new ByteArrayOutputStream(1024);
+            DataOutputStream dos = new DataOutputStream(os, false);
+
+            dos.writeS32((short) data.size());
+
+            Iterator<Map.Entry<String, byte[]>> iterator = data.entrySet().iterator();
+
+            // すべてのKey-Valueを単純にシリアライズする
+            while (iterator.hasNext()) {
+                Map.Entry<String, byte[]> entry = iterator.next();
+                dos.writeString(entry.getKey());
+                dos.writeFile(entry.getValue());
+            }
+
+            return os.toByteArray();
+        } catch (Exception e) {
+            throw new IllegalStateException();
+        }
+    }
+
+    public static Map<String, byte[]> toKeyValue(byte[] buffer) {
+        return toKeyValue(buffer, 0, buffer.length);
+    }
+
+    public static Map<String, byte[]> toKeyValue(byte[] buffer, int offset, int length) {
+        try {
+            ByteArrayInputStream is = new ByteArrayInputStream(buffer, offset, length);
+            DataInputStream dis = new DataInputStream(is, false);
+
+            Map<String, byte[]> result = new HashMap<>();
+
+            final int numData = dis.readS32();
+            for (int i = 0; i < numData; ++i) {
+                String key = dis.readString();
+                byte[] value = dis.readFile();
+
+                result.put(key, value);
+            }
+
+            return result;
+        } catch (Exception e) {
+            LogUtil.log(e);
+            throw new IllegalArgumentException("FormatError");
+        }
+    }
+
+}
