edc1f86e2a14904695c60157c60c428a980883d3
eaglesakura <eagle.sakura@gmail.com>
2016-07-24 00:09:14 +0900

    シリアライズ制限をコメントに追記

--- src/main/java/com/eaglesakura/serialize/Serialize.java
+++ src/main/java/com/eaglesakura/serialize/Serialize.java
@@ -7,6 +7,10 @@ import java.lang.annotation.Target;
 
 /**
  * シリアライズ対象の変数に指定する
+ *
+ * MEMO: プリミティブ型の場合は互換性チェックが働くが、それ以外の場合はフィールドの削除を行うとreadが正常に行えない制限がある。
+ *
+ * そのため、ファイル上にシリアライズされているかつ互換性を保つ場合、不要なフィールドはDeprecatedにし、直接的なフィールド削除を絶対に行わないこと。
  */
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
